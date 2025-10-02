38533685d97de997eeb1304f6e17cea14b6db764
akhfadhil <akhmadfadhil512@gmail.com>
2025-10-03 04:22:38 +0700

    Test login with email & password

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,10 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
     function index () {
-        echo "Halo";
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (Auth::attempt($credentials)) {
+            echo "Sukses";
+        } else {
+            return redirect('/')->withErrors('Invalid email or password')->withInput();
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [SessionController::class, 'index']);
+Route::post('/', [SessionController::class, 'login']);
