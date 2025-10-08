b8c323b3a3e09734514742d532ad7dec354f138e
akhfadhil <akhmadfadhil512@gmail.com>
2025-10-08 07:55:03 +0700

    Fix PPK limitation

--- app/Http/Controllers/KecamatanController.php
+++ app/Http/Controllers/KecamatanController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Kecamatan;
+use Illuminate\Support\Facades\Auth;
 
 class KecamatanController extends Controller
 {
     public function index($id)
     {
+
+
+        $user = Auth::user();
+
+        // Cegah PPK mengakses kecamatan lain
+        if ($user->role->role === 'ppk') {
+            $userKecamatanId = $user->userable->kecamatan_id ?? null;
+
+            if ($userKecamatanId !== (int) $id) {
+                return redirect()->route('kecamatan.index', ['id' => $userKecamatanId])
+                    ->with('error', 'Anda tidak memiliki akses ke kecamatan lain.');
+            }
+        }
+
         $kecamatan = Kecamatan::with('desa')->findOrFail($id);
 
         // dd($kecamatan->nama_kecamatan);
